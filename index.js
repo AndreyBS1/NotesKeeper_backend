34e381177448eca1c63ea9ba4fd4f2186dd1c4d9
AndreyBS1 <andrey.babenkov.00@gmail.com>
2022-03-05 16:45:04 +0300

    Added CRUD routes

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ app
     .get('/', (req, res) => {
         res.send('Server is now working');
     })
+    .get('/noteskeeper/get', (req, res) => {
+        res.send('Get request');
+    })
+    .post('/noteskeeper/post', (req, res) => {
+        res.send('Post request');
+    })
+    .put('/noteskeeper/put', (req, res) => {
+        res.send('Put request');
+    })
+    .delete('/noteskeeper/delete', (req, res) => {
+        res.send('Delete request');
+    })
     .listen(PORT);
