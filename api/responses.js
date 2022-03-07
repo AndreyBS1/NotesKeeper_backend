b447cadcccff381d14c44168e05773bd56c66b33
AndreyBS1 <andrey.babenkov.00@gmail.com>
2022-03-07 19:18:21 +0300

    Added get function v.11

--- api/responses.js
+++ api/responses.js
@@ -1,11 +1,12 @@
 const db = require('../db/models/index');
-const { DataTypes } = require('sequelize');
-const NotesModel = require('../db/models/notes')(db.sequelize, DataTypes);
+// const { DataTypes } = require('sequelize');
+// const NotesModel = require('../db/models/notes')(db.sequelize, DataTypes);
 
 class Response {
     static async get() {
-        const allNotes = await NotesModel.findAll();
-        return JSON.stringify(allNotes, null, 2);
+        // const allNotes = await NotesModel.findAll();
+        // return JSON.stringify(allNotes, null, 2);
+        return typeof(db.models.notes)
     }
 }
 
